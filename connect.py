a15e13b899703064d762dcacd50dcd9234831766
Gabrieli Romagna Rita <gabihromagna@gmail.com>
2025-08-07 16:41:49 -0300

    alteração no connect

--- connect.py
+++ connect.py
@@ -2,9 +2,10 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
+cred = credentials.Certificate("chaveAcesso/loja-face-a-face-firebase-adminsdk-fbsvc-6acafc7c88.json")
 
 app = firebase_admin.initialize_app(cred)
 
 db = firestore.client()
+
+print("Banco conectado!")
