f9a898240615f3d0e01c704d48cc87a167d06e94
gabrieliromagnarita <gabihromagna@gmail.com>
2025-08-16 16:26:58 -0300

    adicionando o Flask

--- cadastro.py
+++ cadastro.py
@@ -1,12 +1,13 @@
+from flask import Flask, render_template, request, redirect
 import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-from firebase_admin import auth
+from firebase_admin import credentials, firestore, auth
 from email_validator import validate_email, EmailNotValidError
 
 cred = credentials.Certificate("chaveAcesso/loja-face-a-face-firebase-adminsdk-fbsvc-6acafc7c88.json")
-app = firebase_admin.initialize_app(cred)
+firebase_admin.initialize_app(cred)
 db = firestore.client()
+
+app = Flask(__name__) #__name__ é o nome do módulo atual
 
 def email_valido(email):
     try:
@@ -14,39 +15,44 @@ def email_valido(email):
         return True
     except EmailNotValidError:
         return False
+    
+@app.route('/')
+def cadastro():
+    return render_template('cadastro.html')
 
+@app.route('/cadastrar', methods=['POST'])
 def cadastrar_user():
-    email = input("Email: ")
-    senha = input("Senha: ")
-    senhaConfirm = input("Confirme a senha: ")
-    nome = input("Nome: ")
-    telefone = input("Telefone: ")
-    nascimento = input("Nascimento: ")
+    email = request.form['email-cadastro']
+    senha = request.form['senha-cadastro']
+    senhaConfirm = request.form['senhaConfirm-cadastro']
+    nome = request.form['nome-cadastro']
+    telefone = request.form['fone-cadastro']
+    nascimento = request.form['dataNasc-cadastro']
 
     if email_valido(email) == False:
-        print("Email inválido!")
+        return "Email inválido!"
     if senhaConfirm != senha:
-        print("Senha inválida!")
-    else:
-        try: 
-            auth.create_user(
-                email = email,
-                password = senha,
-            )
+        return "As senhas não conferem!"
 
-            try:
-                user_info = {
-                    'email': email,
-                    'senha': senha,
-                    'nome': nome,
-                    'telefone': telefone,
-                    'nascimento': nascimento,
-                }
-                db.collection('usuarios').add(user_info)
-                print(f'Usuário criado com sucesso!')
-            except Exception:
-                print(f'Falha na criação do usuário!')
+    try: 
+        user = auth.create_user(
+            email = email,
+            password = senha,
+        )
+
+        try:
+            user_info = {
+                'email': email,
+                'nome': nome,
+                'telefone': telefone,
+                'nascimento': nascimento,
+            }
+            db.collection('usuarios').add(user_info)
+            print(f'Usuário criado com sucesso!')
         except Exception:
-            print(f'Falha na criação do usuário!1')
+            print(f'Falha na criação do usuário!')
+    except Exception:
+        print(f'Falha na criação do usuário!1')        
 
-cadastrar_user()
+if __name__ == "__main__":
+    app.run(debug=True)
