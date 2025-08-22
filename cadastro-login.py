d750491a40971812cda154399be7a91be0315e3d
gabrieliromagnarita <gabihromagna@gmail.com>
2025-08-22 19:06:55 -0300

    front login pronto

--- cadastro-login.py
+++ cadastro-login.py
@@ -1,4 +1,4 @@
-from connect import db
+""" from connect import db """
 from flask import Flask, render_template, request, redirect, url_for
 import firebase_admin
 from firebase_admin import credentials, firestore, auth
