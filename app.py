fad48fa7365cab911b3218b1a0f771df7be25247
Gabrieli Romagna Rita <gabihromagna@gmail.com>
2025-09-03 15:37:29 -0300

    FAQ começado

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from routes.login import login_bp
 from routes.admin import admin_bp
 from routes.rotas import rotas_bp
 from routes.historia import historia_bp
+from routes.faq import faq_bp
+from routes.home import home_bp
 
 app = Flask(__name__)
 
@@ -12,6 +14,8 @@ app.register_blueprint(login_bp)
 app.register_blueprint(rotas_bp)
 app.register_blueprint(admin_bp)
 app.register_blueprint(historia_bp)
+app.register_blueprint(faq_bp)
+app.register_blueprint(home_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
