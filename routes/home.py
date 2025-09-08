6d5a11778947f1ca2f449b1784be2c991bd0a1f8
Gabrieli Romagna Rita <gabihromagna@gmail.com>
2025-09-08 13:15:48 -0300

    home filtro

--- routes/home.py
+++ routes/home.py
@@ -1,21 +1,23 @@
 from flask import Blueprint, render_template, request
-""" from connect import db """
+from connect import db
 
 home_bp = Blueprint('home', __name__)
 
 @home_bp.route('/home-render')
 def home():
-    return render_template('home.html')
-
-def home_filters():
     produtos = [doc.to_dict() for doc in db.collection('produtos').stream()]
+
+    categorias_dict = {}
+    for produto in produtos:
+        for categoria in produto.get('categorias', []):
+            categorias_dict.setdefault(categoria, set())  # ou lista, depende de como seus dados estão
+            # Aqui você pode adicionar subcategorias se houver
+            for sub in produto.get('subcategorias', []):  # supondo que tenha esse campo
+                categorias_dict[categoria].add(sub)
+
+    # Converta sets para listas (Jinja não renderiza sets corretamente)
+    categorias_dict = {k: list(v) for k, v in categorias_dict.items()}
+
+    return render_template('home.html', produtos=produtos, categorias_dict=categorias_dict)
     
-    categoria = request.args.get('categoria')
-    subcategoria = request.args.get('subcategoria')
-
-    if categoria:
-        produtos = [i for i in produtos if categoria in i.get('categorias', [])]
-    if subcategoria:
-        produtos = [i for i in produtos if subcategoria in i.get('categorias', [])]
-
-    return render_template("home.html", produtos=produtos)
+    
