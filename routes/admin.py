323cd6dc53b5a6416e41947915104543b5d03fcc
Gabrieli Romagna Rita <gabihromagna@gmail.com>
2025-09-10 14:40:22 -0300

    filtros e home

--- routes/admin.py
+++ routes/admin.py
@@ -1,5 +1,5 @@
+from connect import db
 from flask import Blueprint, render_template, request, redirect, url_for
-""" from connect import db """
 import uuid
 import os
 
--- routes/home.py
+++ routes/home.py
@@ -1,23 +1,17 @@
-from flask import Blueprint, render_template, request
 from connect import db
+from flask import Blueprint, render_template, request
 
 home_bp = Blueprint('home', __name__)
 
 @home_bp.route('/home-render')
 def home():
-    produtos = [doc.to_dict() for doc in db.collection('produtos').stream()]
+    categoria = request.args.get('categoria')
 
-    categorias_dict = {}
-    for produto in produtos:
-        for categoria in produto.get('categorias', []):
-            categorias_dict.setdefault(categoria, set())  # ou lista, depende de como seus dados estão
-            # Aqui você pode adicionar subcategorias se houver
-            for sub in produto.get('subcategorias', []):  # supondo que tenha esse campo
-                categorias_dict[categoria].add(sub)
+    produtos = []
 
-    # Converta sets para listas (Jinja não renderiza sets corretamente)
-    categorias_dict = {k: list(v) for k, v in categorias_dict.items()}
-
-    return render_template('home.html', produtos=produtos, categorias_dict=categorias_dict)
-    
-    
+    if categoria:
+        produtos_ref = db.collection('produtos')
+        produtos = [doc.to_dict() for doc in produtos_ref.where('categorias', 'array_contains', categoria.lower()).stream()]
+    else:
+        produtos = [doc.to_dict() for doc in db.collection('produtos').stream()]
+    return render_template('home.html', produtos=produtos, categoria_atual=categoria)
