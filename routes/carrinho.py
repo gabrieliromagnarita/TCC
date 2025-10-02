8d8a76695cb5533d83ec8cc0307f75d76a50dc05
Gabrieli Romagna Rita <gabihromagna@gmail.com>
2025-10-02 17:26:53 -0300

    adicionando produtos e fazendo carrinho

--- routes/carrinho.py
+++ routes/carrinho.py
@@ -3,40 +3,44 @@ from flask import Blueprint, render_template, request, redirect, url_for, sessio
 import firebase_admin
 from firebase_admin import credentials, firestore, auth
 import requests
+from collections import Counter
 
 carrinho_bp = Blueprint('carrinho', __name__)
 FIREBASE_API_KEY = 'AIzaSyB83_gLrndTGy1mx5jG8CJEA_LrCsCijdw'
 
 @carrinho_bp.route('/carrinho-render')
 def carrinho():
-    carrinho_cod = session.get('carrinho', [])
+    carrinho_ids = session.get('carrinho', [])
+    contagem = Counter.carrinho_ids()
     produtos_carrinho = []
-    total = 0
+    total = 0.0
 
-    for produto_id in carrinho_cod:
-        produto_ref = db.collection("produtos").document(produto_id).get()
-        if produto_ref:
-            produto = produto_ref.to_dict()
-            produto["cod"] = produto_id
-            produtos_carrinho.append(produto)
-            total += produto.get("preco", 0)
+    for produto_id, qtd in contagem.items():
+        doc = db.collection("produtos").document(produto_id).get()
+        if not doc.exists:
+            continue
+        produto = doc.to_dict()
+        produto["id"] = produto_id
+        produto["quantidade"] = qtd
+        produtos_carrinho.append(produto)
+        total += produto.get("preco", 0)
     
     return render_template('carrinho.html', produtos = produtos_carrinho, total = total)
 
-@carrinho_bp.route('/remove_carrinho/<produto_id>')
+@carrinho_bp.route('/remove_carrinho/<produto_id>', methods=['POST','GET'])
 def remove_carrinho(produto_id):
-    if carrinho in session and produto_id in session['carrinho']:
-        session['carrinho'].remove(produto_id)
-        session.modified = True
-    return(redirect(url_for('carrinho.carrinho')))
+    produto_id = str(produto_id)
+    carrinho = session.get('carrinho', [])
+    
+    if produto_id in carrinho:
+        carrinho.remove(produto_id)
+        session['carrinho'] = carrinho
+    return(redirect(request.referrer or url_for('carrinho.carrinho')))
 
 @carrinho_bp.route('/add_carrinho/<produto_id>')
 def add_carrinho(produto_id):
-    if 'carrinho' not in session:
-        session['carrinho'] = []
-
+    produto_id = str(produto_id)
+    session.setdefault('carrinho',[])
     session['carrinho'].append(produto_id)
     session.modified = True
-
-    # volta para a página anterior (em vez de ir para o carrinho)
-    return redirect(request.referrer)
+    return(redirect(request.referrer or url_for('produto.produto', id="produto_id")))
