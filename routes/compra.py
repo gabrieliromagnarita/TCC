6ca85d3e675269e75ed2d6494188bed521253945
gabrieliromagnarita <gabihromagna@gmail.com>
2025-10-22 11:21:40 -0300

    back compras pix e boleto

--- routes/compra.py
+++ routes/compra.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, session, redirect, url_for
+from flask import Blueprint, render_template, session, redirect, url_for, request, flash
+import uuid, datetime, qrcode, base64, barcode
+from io import BytesIO
+from barcode.writer import ImageWriter
 
 compra_bp = Blueprint('compra', __name__)
 
@@ -12,3 +15,42 @@ def compra():
     total = compra_data.get('total', 0.0)
 
     return render_template('compra.html', produtos=produtos, total=total)
+
+@compra_bp.route('/comprar', methods=['post'])
+def comprar():
+    tipo = request.form.get('tipo-pag')
+    compra_data = session.get('compra')
+
+    if not compra_data:
+        return redirect(url_for('carrinho.carrinho'))
+    
+    produtos = compra_data.get('produtos', [])
+    total = compra_data.get('total', 0,0)
+    pedido_cod = str(uuid.uuid4())[:8]
+
+    pag_info = None
+
+    if tipo == "pix":
+        chave = '@gmail.com'
+        qr_code = ''
+        qrcode_img = qrcode.make(qr_code)
+        buffer = BytesIO() #
+        qrcode_img.save(buffer, format="png") #
+        img_b64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
+
+
+        pag_info = {"tipo":"pix", "pedido_cod": pedido_cod, "chave": chave, "qrcode":qr_code, "qrcode_img": img_b64}
+
+    elif tipo == "boleto":
+        vencimento = (datetime.date.today() + datetime.timedelta(days=3)).strftime("%d/%m/%Y")
+        cod_barras = ''
+
+        codigo = pag_info["cod_barras"].replace(" ","")
+        barcode = barcode.get('code128', codigo, writer=ImageWriter())
+        buffer = BytesIO()
+        barcode.write(buffer)
+        cod_barras_img = base64.b64encode(buffer.getvalue()).decode("utf-8")
+
+        pag_info = {"tipo":"boleto", "pedido_cod": pedido_cod, "vencimento": vencimento, "cod_barras": cod_barras, "cod_barras_img": cod_barras_img}
+
+    return render_template('compra.html', produtos=produtos, total=total, pag_info=pag_info)
